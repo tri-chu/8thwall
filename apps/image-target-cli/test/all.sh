ea6f3e8db3bcc014d56c18e464f95fe63d9cf3ae
Christoph Bartschat <christoph@8thwall.com>
2026-02-24 17:53:24 -0800

    [image-target-cli] Fix override option in test script

--- apps/image-target-cli/test/all.sh
+++ apps/image-target-cli/test/all.sh
@@ -3,8 +3,7 @@ set -e
 
 cd $(dirname "$0")/..
 
-OVERWRITE_FILES=true
 for file in test/*.txt; do
-  echo "Running OVERWRITE_FILES=true cat $file | node ./src/index.js"
-  cat "$file" | node ./src/index.js >/dev/null
+  echo "Running cat $file | OVERWRITE_FILES=true node ./src/index.js"
+  cat "$file" | OVERWRITE_FILES=true node ./src/index.js >/dev/null
 done
