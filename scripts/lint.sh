b92fc3dfb05b61a0040c593ae5895df582020955
Christoph Bartschat <christoph@8thwall.com>
2026-05-01 12:07:42 -0700

    Exclude deleted files in lint.sh checks (#108)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,8 +5,8 @@ set -eu
 cd "$(dirname "$0")/.."
 
 # Get list of changed files
-BAZEL_FILES=$(git diff --name-only $(git merge-base main HEAD) | grep -E "(WORKSPACE|\/BUILD|\.bzl|\.bazel)$" || true)
-JS_FILES=$(git diff --name-only $(git merge-base main HEAD) | grep -E "\.[tj]sx?$" | grep -v "ecs/resources" || true)
+BAZEL_FILES=$(git diff --name-only --diff-filter=MAR $(git merge-base main HEAD) | grep -E "(WORKSPACE|\/BUILD|\.bzl|\.bazel)$" || true)
+JS_FILES=$(git diff --name-only  --diff-filter=MAR $(git merge-base main HEAD) | grep -E "\.[tj]sx?$" | grep -v "ecs/resources" || true)
 
 # Only run commands if there are matching files
 if [[ -n "$BAZEL_FILES" ]]; then
